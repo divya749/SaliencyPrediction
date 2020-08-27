e299af8327f40479f205bdf508e13ca976076af9
Divya Singh <43903617+divya749@users.noreply.github.com>
2020-08-27 14:15:54 +0530

    Add files via upload

---
+++ conversions/e2c.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+from e2c import utils
+
+
+def e2c(e_img, cube_format):
+    '''
+    e_img:  ndarray in shape of [H, W, *]
+    face_w: int, the length of each face of the cubemap
+    '''
+    face_w=256
+    mode ='bilinear'
+    
+    #assert len(e_img.shape) == 3
+    h, w = e_img.shape[:2]
+    if mode == 'bilinear':
+        order = 1
+    elif mode == 'nearest':
+        order = 0
+    else:
+        raise NotImplementedError('unknown mode')
+
+    xyz = utils.xyzcube(face_w)
+    uv = utils.xyz2uv(xyz)
+    coor_xy = utils.uv2coor(uv, h, w)
+
+    cubemap = np.stack([
+        utils.sample_equirec(e_img[..., i], coor_xy, order=order)
+        for i in range(e_img.shape[2])
+    ], axis=-1)
+
+    if cube_format == 'horizon':
+        pass
+    elif cube_format == 'list':
+        cubemap = utils.cube_h2list(cubemap)
+    elif cube_format == 'dict':
+        cubemap = utils.cube_h2dict(cubemap)
+    elif cube_format == 'dice':
+        cubemap = utils.cube_h2dice(cubemap)
+    else:
+        raise NotImplementedError()
+
+    return cubemap
